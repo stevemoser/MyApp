f679da50ca1a23282503d79f511ca1410000b6d0
Steve Moser <contact@stevemoser.org>
2018-06-19 15:10:49 -0700

    init

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var duckDuckGo = DuckDuckGo()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        duckDuckGo.performSearch(for: "steve jobs") { definition in
+            if let definition = definition {
+                print(definition)
+            } else {
+                print("error")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
